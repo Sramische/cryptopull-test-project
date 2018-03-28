871ff5c023a690c9002d75dc0a92b39d461106b5
Sramische <sramische@gmail.com>
2018-03-29 00:31:23 +0300

    Removed breakpoint

--- cryptopull_api/app/jobs/coin_status_request_job.rb
+++ cryptopull_api/app/jobs/coin_status_request_job.rb
@@ -8,7 +8,6 @@ class CoinStatusRequestJob < ApplicationJob
     end
 
     def perform(arg = nil)
-        byebug
         arr = CoinApi.fetch_coin_stats
         HistoryEntry.bulk_insert(arr)
     end
